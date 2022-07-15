b72dcebefe9760bd2aa34cd3c017ac0428d865e6
tlfinn1618 <tlfinn1618@gmail.com>
2022-07-15 13:22:52 -0700

    Reverted to file af006d522e8074c6a9d2ed91402561c4aff34a84 on my_file.py

--- my_file.py
+++ my_file.py
@@ -1,11 +1,6 @@
 print('Hello world')
 
-print('Remove me!')
-
 print('Hi another feature')
-
-def my_funct :
-    return 'This is yet another feature'
 
 def my_funct :
     return 'This is yet another feature'
